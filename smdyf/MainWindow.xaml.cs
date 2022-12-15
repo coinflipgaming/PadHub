266ad2b5e2810aa93ce7c2658cab9be76f342506
student <student@s203-pc15>
2022-12-15 13:16:19 +0100

    Dodaj pliki projektów.

---
+++ smdyf/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using smdyf.Windows;
+using System.Windows;
+
+namespace smdyf
+{
+    /// <summary>
+    /// Logika interakcji dla klasy LoginWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Calculator(object sender, RoutedEventArgs e)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Show();
+            this.Close();
+        }
+
+        private void Button_Click_TicTacToe(object sender, RoutedEventArgs e)
+        {
+
+            TicTacToe TicTacToe = new TicTacToe();
+            TicTacToe.Show();
+            this.Close();
+        }
+
+        private void Button_Click_MemoCards(object sender, RoutedEventArgs e)
+        {
+            MemoCards memoCards = new MemoCards();
+            memoCards.Show();
+            this.Close();
+        }
+    }
+}
---
+++ smdyf/Windows/TicTacToe.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace smdyf.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TicTacToe.xaml
+    /// </summary>
+    public partial class TicTacToe : Window
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+        private void Button_Powrot(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
