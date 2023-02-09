d00c024030483c0a6f558cccfcfacb1d3635c3ba
Damian Mika <terarionator@wp.pl>
2023-02-09 14:22:52 +0100

    gotowe :D

--- smdyf/Windows/Calculator.xaml.cs
+++ smdyf/Windows/Calculator.xaml.cs
@@ -2,20 +2,11 @@
 using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Data;
-using System.Linq;
 
 namespace smdyf.Windows
 {
-    /// <summary>
-    /// Logika interakcji dla klasy Calculator.xaml
-    /// </summary>
     public partial class Calculator : Window
     {
-        String num1 = "";
-        String num2 = "";
-        String current_symbol = "";
-        bool newcalc = true;
         public Calculator()
         {
             InitializeComponent();
@@ -36,73 +27,27 @@ namespace smdyf.Windows
                 
                 switch (inpt)
                 {
-
-                    case "-":
-                        change_symbol(inpt);
-                        break;
-                    case "+":
-                        change_symbol(inpt);
-                        break;
-                    case "*":
-                        change_symbol(inpt);
-                        break;
-                    case "/":
-                        change_symbol(inpt);
-                        break;
-                    case "%":
-                        change_symbol(inpt);
+                    case "→":
+                        textbox.Text = textbox.Text.Substring(0, textbox.Text.Length - 1);
                         break;
                     case "+/-":
-                        if (textbox.Text.StartsWith("-")) { textbox.Text = textbox.Text.Substring(1); break; }
-                        textbox.Text = "-" + textbox.Text;
+                        textbox.Text += "*(-1)";
                         break;
                     case "√":
-                        if (textbox.Text.StartsWith("-")) { break; }
                         textbox.Text = Math.Sqrt(float.Parse(textbox.Text)).ToString();
                         break;
                     case "=":
-                        if(current_symbol == ""){break;}
-                        if(num1 == ""){break;}
-                        num2 = textbox.Text.ToString();
-                        if(num2 == ""){break;}
-
-                        string math = num1+current_symbol+num2;
+                        string math = textbox.Text.ToString();
                         string value = new DataTable().Compute(math, null).ToString();
-
-                        textbox.Text = value;
-                        num1 = value;
-                        num2 = "";
-                        newcalc = true;
+                        textbox.Text = value.Replace(",", "."); ;
                         break;
                     case "AC":
-                        textbox.Text = "0";
-                        num1 = "0";
-                        num2 = "0";
-                        current_symbol = "";
-                        newcalc = true;
+                        textbox.Text = "";
                         break;
                     default:
-                        if (newcalc) { newcalc = false; textbox.Text = ""; }
-                        if(textbox.Text == "0")
-                        {
-                            textbox.Text = inpt;
-                            break;
-                        }
+                        if (textbox.Text.ToString() == "0") { textbox.Text = inpt; break; }
                         textbox.Text += inpt;
                         break;
-                }
-                void change_symbol(String symbol)
-                {
-                    if (textbox.Text.ToString() == "")
-                    {
-                        return;
-                    }
-                    if (num1 == "")
-                    {
-                        num1 = textbox.Text.ToString();
-                        textbox.Text = "";
-                    }
-                    current_symbol = symbol;
                 }
             }
             catch
