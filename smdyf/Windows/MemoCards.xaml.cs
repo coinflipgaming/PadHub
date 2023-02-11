fcf35c06cfd9fe6d49f6503abe50c70873b45759
Damian Mika <terarionator@wp.pl>
2023-02-12 00:04:26 +0100

    memocards finished

--- smdyf/Windows/MemoCards.xaml.cs
+++ smdyf/Windows/MemoCards.xaml.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace smdyf.Windows
 {
     public partial class MemoCards : Window
     {
-        public IList<card> Deck = new List<card>();
-        public int cards_flipped = 0;
+        public List<card> Deck = new List<card>();
+        public List<card> clicked = new List<card>();
+        public int moves = 0;
+        public int pts = 0;
+        public int _rows = 3;
+        public int _columns = 3;
         public MemoCards()
         {
             InitializeComponent();
             new_game_button.Click += (s, e) =>
             {
                 MemoCardsDialog window = new MemoCardsDialog(this);
+                window.rows.Text = _rows.ToString();
+                window.columns.Text = _columns.ToString();
                 window.Show();
             };
         }
@@ -35,10 +33,15 @@ namespace smdyf.Windows
         }
         public void new_game(int Rows = 0,int Columns = 0)
         {
+            if (Rows!=0) { _rows = Rows; }
+            if (Columns != 0) { _columns = Columns; }
+            moves = 0;
+            clicked.Clear();
             Deck.Clear();
             container.Children.Clear();
             container.RowDefinitions.Clear();
             container.ColumnDefinitions.Clear();
+            Random rnd = new();
             for (int i = Rows; i > 0; i--)
             {
                 container.RowDefinitions.Add(new RowDefinition());
@@ -47,15 +50,58 @@ namespace smdyf.Windows
             {
                 container.ColumnDefinitions.Add(new ColumnDefinition());
             }
-            for (int i = Rows; i >= 0; i--)
+            List<int> unassigned = new List<int>();
+            for (int i = 0; (Columns*Rows/2) > i; i++)
             {
-                for (int j = Columns; j >= 0; j--)
+                unassigned.Add(i+1);
+                unassigned.Add(i+1);
+            }
+            if (Columns*Rows%2!=0) { unassigned.Add(0); }
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
                 {
-                    card _card = new card();
+                    card _card = new();
+                    _card.id = Deck.Count();
                     _card.x = i;
                     _card.y = j;
-                    Button _button = new Button();
-                    _button.Content = "x: " + i.ToString() + " y: " + j.ToString();
+                    Button _button = new();
+                    int _index = rnd.Next(unassigned.Count()-1);
+                    _card.pair_id = unassigned[_index];
+                    unassigned.RemoveAt(_index);
+                    _button.Click += (s, e) =>
+                    {
+                        moves += 1;
+                        clicked.Add(_card);
+                        _button.Content = _card.pair_id.ToString();
+                        _button.IsEnabled = false;
+                        if (clicked.Count() == 2)
+                        {
+                            if (clicked[0].pair_id == clicked[1].pair_id)
+                            {
+                                Deck.Remove(clicked[0]);
+                                Deck.Remove(clicked[1]);
+                                clicked.Clear();
+                                pts += 10;
+                            }
+                        }
+                        else if (clicked.Count() == 3)
+                        {
+                            clicked[0].button.IsEnabled = true;
+                            clicked[0].button.Content = "";
+                            clicked.RemoveAt(0);
+                            clicked[0].button.IsEnabled = true;
+                            clicked[0].button.Content = "";
+                            clicked.RemoveAt(0);
+                            pts -= 1;
+                        }
+                        points.Content = "ruchy: " + moves.ToString() + " punkty: " + pts.ToString();
+                        if (Deck.Count() == 0)
+                        {
+                            MessageBox.Show("Wygrałeś! " + " ruchy: " + moves.ToString() + " punkty: " + pts.ToString());
+                            new_game();
+                        }
+                    };
                     _card.button = _button;
                     Deck.Add(_card);
                     Grid.SetRow(_button, i);
@@ -63,25 +109,12 @@ namespace smdyf.Windows
                     container.Children.Add(_button);
                 }
             }
-            int n = 0;
-            while (2 < Deck.Count())
-            {
-                n += 1;
-                Random rand = new();
-                int a = rand.Next(0, Deck.Count()-1);
-                Deck[a].pair_id = n;
-                Deck[a].button.Content = n.ToString();
-                Deck.RemoveAt(a);
-                int b = rand.Next(0, Deck.Count()-1);
-                Deck[b].pair_id = n;
-                Deck[b].button.Content = n.ToString();
-                Deck.RemoveAt(b);
-
-            }
+            if (Columns * Rows % 2 != 0) { Deck.RemoveAt(Deck.Count() - 1); container.Children.RemoveAt(Deck.Count()); }
         }
-    }
+}
     public class card
-    {
+    {   
+        public int id { get; set; }
         public int pair_id { get; set; }
         public int x { get; set; }
         public int y { get; set; }
